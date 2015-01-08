d999730b15da54c192bd99dbe1eb73fbd848fff3
Ed Sinjiashvili <ed.sinjiashvili@gmail.com>
2015-01-08 23:17:56 +0300

    screen bounds checking

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, sprite) {
+var Enemy = function(x, y, filename) {
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
   this.x = x || 100;
@@ -7,7 +7,10 @@ var Enemy = function(x, y, sprite) {
 
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
-  this.sprite = sprite || 'images/enemy-bug.png';
+  this.sprite = Resources.get(filename || 'images/enemy-bug.png');
+  
+  this.w = this.sprite.width;
+  this.h = this.sprite.height;
 };
 
 // Update the enemy's position, required method for game
@@ -25,7 +28,7 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(this.sprite, this.x, this.y, this.w, this.h);
 };
 
 // Now write your own player class
@@ -40,6 +43,15 @@ var Player = function (x, y) {
     up:    false,
     down:  false
   };
+
+  this.shrink = false;
+  this.max_w  = this.w * 2;
+  this.min_w  = this.w / 2;
+
+  this.do_shrink = false;
+
+
+  this.screen_bounds = {x: 0, y: 0, w: 500,  h: 600};
 };
 
 Player.prototype = Object.create(Enemy.prototype);
@@ -60,6 +72,51 @@ Player.prototype.update = function(dt) {
 
   if (this.moves.down) {
     this.y += 10;
+  }
+
+  this.force_screen_bounds();
+
+  if (this.do_shrink) {
+    var shrink_rate = 4;
+    if (this.shrink) {
+      this.w -= shrink_rate;
+      this.h -= shrink_rate;
+      if (this.w < this.min_w) {
+        this.shrink = false;
+      }
+    } else {
+      this.w += shrink_rate;
+      this.h += shrink_rate;
+      if (this.w > this.max_w) {
+        this.shrink = true;
+      }
+    }
+  }
+};
+
+Player.prototype.right = function() {
+  return this.x + this.w;
+};
+
+Player.prototype.bottom = function() {
+  return this.y + this.h;
+};
+
+Player.prototype.force_screen_bounds = function() {
+  if (this.x < this.screen_bounds.x) {
+    this.x = this.screen_bounds.x;
+  }
+
+  if (this.right() > this.screen_bounds.w) {
+    this.x = this.screen_bounds.w - this.w;
+  }
+
+  if (this.y < this.screen_bounds.y) {
+    this.y = this.screen_bounds.y;
+  }
+
+  if (this.bottom() > this.screen_bounds.h) {
+    this.y = this.screen_bounds.h - this.h;
   }
 };
 
